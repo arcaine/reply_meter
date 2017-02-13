ce4c1874d9ebd045a1cba7ce716715dcb2e21544
MINSOO\minsoo <arcaine34@gmail.com>
2017-02-13 18:54:17 +0900

    기사 리스트 긁어오기

---
+++ tester/list_parser.js
@@ -0,0 +1,50 @@
+var request = require("request");
+var cheerio = require('cheerio');
+var iconv  = require('iconv-lite');
+
+var url = "http://news.naver.com/main/main.nhn?mode=LSD&mid=shm&sid1=100#&date=2017-02-13 00:00:00&page=1"
+
+var requestOptions  = { method: "GET"
+                ,uri: url
+                ,headers: { "User-Agent": "Mozilla/5.0" }
+                ,encoding: null
+            };
+
+var getid = function(a){
+ var b = a.toString()
+ var c = b.substring(b.length,b.length-10)
+ return c
+}
+
+// request 모듈을 이용하여 html 요청
+var result = {}
+request(requestOptions, function(error, response, body) {
+  // 전달받은 결과를 EUC-KR로 디코딩하여 출력한다.
+  var strContents = new Buffer(body);
+  var body = iconv.decode(strContents, 'EUC-KR').toString();
+  var $ = cheerio.load(body);
+  var postlists = $("div.section_body ul");
+  postlists.each(function(){
+    var article_lists = $(this).find("li")
+    article_lists.each(function(){
+      var article_name = $(this).find("a").text().trim();
+      var article_company = $(this).find("span.writing").text().trim();
+      var article_url = $(this).find("a").attr("href");
+      var article_id = getid(article_url)
+      // console.log(article_id)
+      // console.log("title: "+article_name)
+      // console.log("company: "+article_company)
+      // console.log("url: "+article_url)
+      var a = {
+        title:article_name,
+        company:article_company,
+        url:article_url
+      }
+      result["n"+article_id] = a
+    });
+  });
+  // console.log(body);
+  //일단은 전체 내용은 디코드하기 -> 그다음에 cheerio 처리등을 진행해야 함
+  console.log("finished")
+  console.log(result)
+});
