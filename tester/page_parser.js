2e1230d29ebdf72aaa5a161bbf7a1d3df7e5a5a4
MINSOO\minsoo <arcaine34@gmail.com>
2017-02-13 17:28:36 +0900

    각 페이지별 크롤링

--- tester/page_parser.js
+++ tester/page_parser.js
@@ -18,9 +18,25 @@ request(requestOptions, function(error, response, body) {
   var $ = cheerio.load(body);
   var postlists = $("div.section_headline.headline_subordi");
   postlists.each(function(){
-    var postname = $(this).find("a.compo_linkhead").text();
-    var posturl = $(this).find("a.compo_linkhead").attr("href");
-    console.log(postname+posturl)
+    // var postname = $(this).find("a.compo_linkhead").text();
+    // var posturl = $(this).find("a.compo_linkhead").attr("href");
+    // console.log("topic name is :"+postname+posturl)
+    // var toparticle_photo = $(this).find("dl.sphoto1 dt.photo a").attr("href");
+    // var toparticle_name = $(this).find("dl.sphoto1 dt").not(".photo").find("a").text();
+    // var toparticle_url = $(this).find("dl.sphoto1 dt").not(".photo").find("a").attr("href");
+    // toparticle_name = toparticle_name.trim();
+    // console.log("top article photo is :"+toparticle_photo);
+    // console.log("top article is :"+toparticle_name);
+    // console.log("It is in the:"+toparticle_url);
+    var article_lists = $(this).find("ul li");
+    article_lists.each(function(){
+      var article_name = $(this).find("a").text().trim();
+      var article_url = $(this).find("a").attr("href");
+      var article_company = $(this).find("span.writing").text().trim();
+      console.log("article!:"+article_name);
+      console.log("it is in the!:"+article_url);
+      console.log("and it is from"+article_company);
+    });
   });
   // console.log(body);
   //일단은 전체 내용은 디코드하기 -> 그다음에 cheerio 처리등을 진행해야 함
