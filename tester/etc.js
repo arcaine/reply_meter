853b0a2b0f6a92dd507d65fa3fdfcac04675c53b
minsoo <arcaine34@gmail.com>
2017-03-04 16:47:00 +0900

    다음 리스트 정리 완료( 당일과 전일 시간 다듬기 까지 )

--- tester/etc.js
+++ tester/etc.js
@@ -1,23 +1,30 @@
 // for (var i = 0 ;   i<10 ; i++){
   // console.log(i);
 // }
-
-var merge = function(a,b){
-  var resultObj = {}
-  for (var i in b){
-    a[i] = b[i]
-  }
-  resultObj = a
-  return resultObj
-}
-
- var a = {a: "abc", b: "abfd"}
- var b = {c: "abc", d: "abfd"}
-console.log(merge(a,b))
-
-
-var list = []
-for(i = 0; i<30; i++){
-  list[i] = i
-}
-console.log(list)
+//
+// var merge = function(a,b){
+//   var resultObj = {}
+//   for (var i in b){
+//     a[i] = b[i]
+//   }
+//   resultObj = a
+//   return resultObj
+// }
+//
+//  var a = {a: "abc", b: "abfd"}
+//  var b = {c: "abc", d: "abfd"}
+// console.log(merge(a,b))
+//
+//
+// var list = []
+// for(i = 0; i<30; i++){
+//   list[i] = i
+// }
+// console.log(list)
+var a =201702171529
+var c = a.toString()
+var b = c.split("")
+var d = ["0","0"]
+var b.append(d)
+// var a =Date.parse("2017-02-17T15:29:00")
+console.log(b)
