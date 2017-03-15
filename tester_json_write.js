32ff0801aab675c5ef2684d426500665c14611ae
minsoo <arcaine34@gmail.com>
2017-03-15 20:04:01 +0900

    daum naver 구분 완성 main tester 다듬기

--- tester_json_write.js
+++ tester_json_write.js
@@ -1,8 +1,19 @@
 var daum_list = require("./lib/daum_list.js");
+var naver_list = require("./lib/naver_list.js");
+
 var fs = require('fs');
-daum_list(1,"politics",function(data){
+daum_list(2,"politics",function(data){
   var stringJson = JSON.stringify(data);
-  fs.writeFile('result.json',stringJson,'utf8',function(err){
+  fs.writeFile('./result_daum.json',stringJson,'utf8',function(err){
+    console.log(err);
     console.log('done!');
   })
+});
+
+naver_list(2,"politics",20170312,function(data){
+  var stringJson = JSON.stringify(data);
+  fs.writeFile('./result_naver.json',stringJson,'utf8',function(err){
+    console.log(err)
+    console.log('done!');
+  });
 });
